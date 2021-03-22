890a66380d14cb40654f24d81af2c20b18f4c569
idembele70 <idembele70@gmail.com>
2021-03-22 22:35:15 +0100

    avec for

--- Program.cs
+++ Program.cs
@@ -8,9 +8,22 @@ namespace CsharpCodeWars
         {
             System.Console.WriteLine(NoSpace());
         }
-     static string NoSpace(string input="Hello world how are you today.")
-     {
-         return input.Replace(" ", "");
-     }
+        static string NoSpace(string input = "Hello world how are you today.")
+        {
+            string inputNoSpace = "";
+            char space = ' ';
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == space)
+                {
+                    inputNoSpace += "";
+                }
+                else
+                {
+                    inputNoSpace += input[i];
+                }
+            }
+            return inputNoSpace;
+        }
     }
 }
