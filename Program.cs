7b9aed607246affd2ab46cd1c7bac1fd46c245f1
idembele70 <idembele70@gmail.com>
2021-03-22 22:05:50 +0100

    repeat str

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace CsharpCodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(RepeatStr());
+        }
+        public static String RepeatStr(int n = 2, string s = "Helli")
+        {
+            return new StringBuilder(s.Length * n).Insert(0, s, n).ToString();
+        }
+    }
+}
